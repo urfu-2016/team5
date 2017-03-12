8742d04b9eaa251c091dc5bbb28ac05f74362eb9
dimastark <dstarkdev@gmail.com>
2017-03-12 22:14:16 +0500

    Шаблоны

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const path = require('path');
+
+const express = require('express');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
 
 var index = require('./routes/index');
-var users = require('./routes/quests');
+var quests = require('./routes/quests');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// Настраиваем вьюхи
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/quests', quests);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,28 @@ var config = require('config');
 var router = express.Router();
 
 /* Список квестов */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Список квестов',
-      mode: config.get('mode')
-  });
+router.get('/', function (req, res) {
+    res.render('index', {
+        title: 'Список квестов',
+        mode: config.get('mode'),
+        quests: [
+            {
+                questId: 1,
+                guestImg: "1.img",
+                questName: "Графити",
+                questLikesCount: 4,
+                questDescription: "Лааааа",
+                questTags: ["Тег2", "Тег3", "Тег3"]
+            },
+            {
+                questId: 2,
+                guestImg: "1.img",
+                questName: "Графити",
+                questLikesCount: 4,
+                questDescription: "Лааааа",
+                questTags: ["Тег2", "Тег3", "Тег3"]
+            }
+        ]});
 });
 
 module.exports = router;
