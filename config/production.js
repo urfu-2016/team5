c9bf3051d4d7bb568f5cac52a82c6e29c1dc612c
Pavel <pp-1995@mail.ru>
2017-03-13 00:06:10 +0500

    Заменил подстановку даных с помощью шаблонных строк

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
     port: 80,
     mode: 'production',
-    mongoUri : 'mongodb://' + process.env.mongoLogin + ':' +
-        process.env.mongoPass + '@ds145359.mlab.com:45359/quests-team5'
+    mongoUri : 'mongodb://${process.env.mongoLogin}:${process.env.mongoPass}' +
+        '@ds145359.mlab.com:45359/quests-team5'
 };
