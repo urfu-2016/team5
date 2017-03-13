78c1026fcd84354399b1fd458d1e529ebe018577
Антонов Алексей <aleksei193@mail.ru>
2017-03-13 15:53:49 +0500

    Настроил линтинг (#14)
    Настроил линтинг

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
-var express = require('express');
-var config = require('config');
-var router = express.Router();
+/* eslint-disable new-cap */
+
+const express = require('express');
+const config = require('config');
+const router = express.Router();
 
 /* Список квестов */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Список квестов',
-      mode: config.get('mode')
-  });
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Список квестов', mode: config.get('mode')});
 });
 
 module.exports = router;
--- routes/quests.js
+++ routes/quests.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var router = express.Router();
+/* eslint-disable new-cap */
+
+const express = require('express');
+const router = express.Router();
 
 /* Возвращаем конкретный квест */
-router.get('/quest/:id', function(req, res, next) {
-  res.render('quest', { title: 'Список квестов' });
+router.get('/quest/:id', function (req, res) {
+    res.render('quest', {title: 'Список квестов'});
 });
 
 module.exports = router;
