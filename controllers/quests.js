8dceb48c5df12668e2e04b614837c2c79c90e456
Pavel <pp-1995@mail.ru>
2017-03-14 11:17:35 +0500

    Убрал присвоение статус кодов и вынес send error в функцию

--- controllers/quests.js
+++ controllers/quests.js
@@ -1,94 +1,53 @@
 'use strict';
 const Quest = require('../models/quest');
 
+function checkError(res, err) {
+    if (err) {
+        res.send(err);
+    }
+}
+
 module.exports = {
     createQuest: (req, res) => {
         const quest = new Quest({
             title: req.body.title,
             description: req.body.description
         });
-        quest.save((err) => {
-            if (!err) {
-                return res.send({ status: 'OK', quest });
-            } else {
-                if(err.name === 'ValidationError') {
-                    res.statusCode = 400;
-                    res.send({ error: 'Validation error' });
-                } else {
-                    res.statusCode = 500;
-                    res.send({ error: 'Server error' });
-                }
-            }
+        quest.save((err, quest) => {
+            checkError(res, err);
+            res.send({ quest });
         });
     },
     getQuests: (req, res) => {
-        return Quest.find((err, quests) => {
-            if (!err) {
-                return res.send(quests);
-            } else {
-                res.statusCode = 500;
-
-                return res.send({ error: 'Server error' });
-            }
+        Quest.find((err, quests) => {
+            checkError(res, err);
+            res.send(quests);
         });
     },
     getQuestById: (req, res) => {
-        return Quest.findById(req.params.id, function (err, quest) {
-            if(!quest) {
-                res.statusCode = 404;
-
-                return res.send({ error: 'Not found' });
-            }
-            if (!err) {
-                return res.send({ status: 'OK', _id: quest.id });
-            } else {
-                res.statusCode = 500;
-
-                return res.send({ error: 'Server error' });
-            }
+        Quest.findById(req.params.id, function (err, quest) {
+            checkError(res, err);
+            res.send({ quest });
         });
     },
     updateQuest: (req, res) => {
-        return Quest.findById(req.params.id, function (err, quest) {
-            if(!quest) {
-                res.statusCode = 404;
-
-                return res.send({ error: 'Not found' });
-            }
+        Quest.findById(req.params.id, function (err, quest) {
+            checkError(res, err);
             quest.title = req.body.title;
             quest.description = req.body.description;
 
-            return quest.save(function (err) {
-                if (!err) {
-                    return res.send({ status: 'OK', quest:quest, message: 'Quest updated!'});
-                } else {
-                    if(err.name === 'ValidationError') {
-                        res.statusCode = 400;
-                        res.send({ error: 'Validation error' });
-                    } else {
-                        res.statusCode = 500;
-                        res.send({ error: 'Server error' });
-                    }
-                }
+            quest.save(function (err, quest) {
+                checkError(res, err);
+                res.send({ quest, message: 'Quest updated!' });
             });
         });
     },
     removeQuest: (req, res) => {
-        return Quest.findById(req.params.id, function (err, quest) {
-            if(!quest) {
-                res.statusCode = 404;
-
-                return res.send({ error: 'Not found' });
-            }
-
-            return quest.remove(function (err) {
-                if (!err) {
-                    return res.send({ status: 'OK', message: 'Quest removed!'});
-                } else {
-                    res.statusCode = 500;
-
-                    return res.send({ error: 'Server error' });
-                }
+        Quest.findById(req.params.id, function (err, quest) {
+            checkError(res, err);
+            quest.remove(function (err) {
+                checkError(res, err);
+                res.send({ message: 'Quest removed!' });
             });
         });
     }
