a321d802cf6cab175de865090206e3f9427be8cf
Мозалов Павел <pp-1995@mail.ru>
2017-03-17 23:35:17 +0500

    Прикрутил Surge (#16) (#40)

--- routes/quests.js
+++ routes/quests.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 /* Возвращаем конкретный квест */
 router.get('/quest/:id', function (req, res) {
-    // TODO: Нужно брать квест по req.id и рендерить в шаблоне quest
+    // Нужно брать квест по req.id и рендерить в шаблоне quest
     res.render('quest', {title: 'Квест'});
 });
 
