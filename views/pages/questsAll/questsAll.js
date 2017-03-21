13463a322c89e038d3bb541b8fb93a774248c53f
Antonov Alexey <aleksei193@mail.ru>
2017-03-21 19:50:29 +0500

    Правки №3

--- views/pages/questsAll/questsAll.js
+++ views/pages/questsAll/questsAll.js
@@ -2,4 +2,3 @@ require('../../blocks/base.css');
 require('../../blocks/questCard/questCard.css');
 require('../../blocks/header/header.css');
 require('./questsAll.css');
- 
--- views/pages/questsId/quests-id.js
+++ views/pages/questsId/quests-id.js
@@ -2,4 +2,3 @@ require('../../blocks/base.css');
 require('../../blocks/comments/comments.css');
 require('../../blocks/header/header.css');
 require('./quests-id.css');
- 
