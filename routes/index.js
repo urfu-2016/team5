fbcdfa886f3cf0b3a2cd193592d3ceb7ba322dbc
Pavel <pp-1995@mail.ru>
2017-03-27 23:43:14 +0500

    Добавил роут для поиска

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const router = express.Router();
 const fs = require('fs');
 const quests = fs.readFileSync('./mocks/quests.json', 'utf-8');
 const questsData = JSON.parse(quests);
+const questsSearchController = require('../controllers/questsSearch');
 
 /* Список квестов */
 router.get('/', function (req, res) {
     // Брать из базы
     res.render('questsAll/quests-all', questsData);
 });
+router.get('/search', questsSearchController.getFoundQuests);
 
 module.exports = router;
--- routes/quests.js
+++ routes/quests.js
@@ -10,14 +10,6 @@ const quest = fs.readFileSync('./mocks/quest.json');
 const questData = JSON.parse(quest);
 const questsController = require('../controllers/api/quests');
 
-/* Возвращаем конкретный квест */
-
-router.route('/quest/:id')
-    .get(function (req, res) {
-    // Нужно брать квест по req.id и рендерить в шаблоне quest
-        res.render('quest', {title: 'Квест'});
-    });
-
 router.route('/api/quests')
     .get(questsController.getQuests)
     .post(questsController.createQuest);
@@ -31,5 +23,7 @@ router.get('/quests/:id', function (req, res) {
     // TODO: Нужно брать квест по req.id и рендерить в шаблоне quest
     res.render('questsId/quests-id', questData);
 });
+
+router.get('/quests/');
 
 module.exports = router;
