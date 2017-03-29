5d1fb64b6695b7229781c00e0326a9c30f7ecdd8
Дмитрий Старков <dstarkdev@gmail.com>
2017-03-29 21:45:45 +0500

    Добавил апи пользователей (#60)

--- controllers/api/quests.js
+++ controllers/api/quests.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use strict';
 
 const HttpStatus = require('http-status-codes');
@@ -36,3 +37,53 @@ module.exports = {
     removeQuest: (req, res) => resolveRequestPromise(Quest.removeBySlug(req.params.slug), res)
 
 };
+=======
+'use strict';
+
+const HttpStatus = require('http-status-codes');
+const Quest = require('../../models/quest');
+const baseApi = require('./baseApi');
+
+module.exports = {
+    createQuest(req, res) {
+        const quest = {
+            title: req.body.title,
+            description: req.body.description,
+            slug: req.body.slug || ''
+        };
+
+        return Quest.create(quest)
+            .then(baseApi.getSuccessCallback(res, HttpStatus.CREATED))
+            .catch(baseApi.getErrorCallback(res));
+    },
+
+    getQuests(req, res) {
+        return Quest.getAll()
+            .then(baseApi.getSuccessCallback(res, HttpStatus.OK))
+            .catch(baseApi.getErrorCallback(res, HttpStatus.NOT_FOUND));
+    },
+
+    getQuestBySlug(req, res) {
+        return Quest.getBySlug(req.params.slug)
+            .then(baseApi.getSuccessCallback(res, HttpStatus.OK))
+            .catch(baseApi.getErrorCallback(res, HttpStatus.NOT_FOUND));
+    },
+
+    updateQuest(req, res) {
+        const questData = {
+            slug: req.body.slug,
+            title: req.body.title,
+            description: req.body.description
+        };
+        return Quest.update(req.body.slug, questData)
+            .then(baseApi.getSuccessCallback(res, HttpStatus.OK))
+            .catch(baseApi.getErrorCallback(res, HttpStatus.NOT_FOUND));
+    },
+
+    removeQuest(req, res) {
+        return Quest.removeBySlug(req.params.slug)
+            .then(baseApi.getSuccessCallback(res, HttpStatus.OK))
+            .catch(baseApi.getErrorCallback(res, HttpStatus.NOT_FOUND));
+    }
+};
+>>>>>>> Добавил апи пользователей (#60)
