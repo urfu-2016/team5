5f8f1e43b171cdaa519e4920a1ffb5b3b784bfca
Alex Solovyev <alexsprint27@mail.ru>
2017-03-30 22:37:35 +0500

    Убрал лишние для этого ПР файлы

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable new-cap */
-
-const router = require('express').Router();
-const authController = require('../controllers/auth');
-
-router.route('/login')
-    .post(authController.authenticate);
-
-module.exports = router;
