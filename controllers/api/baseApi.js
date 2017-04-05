19e54dc8a1c2df1bfd04bc93ec01e0324421de68
Александр Соловьев <alexsprint27@mail.ru>
2017-04-06 00:27:40 +0500

    Base api (#87)

--- controllers/api/baseApi.js
+++ controllers/api/baseApi.js
@@ -2,51 +2,34 @@
 
 const HttpStatus = require('http-status-codes');
 
+function sendResponse(res, status, data) {
+    res.status(status).send(data);
+}
+
 function getSuccessCallback(res, status) {
-    return data => {
-        res
-            .status(status)
-            .send({data, message: HttpStatus.getStatusText(status)});
-    };
+    return data => sendResponse(res, status, {data, message: HttpStatus.getStatusText(status)});
 }
 
-function getErrorCallback(res, httpStatus) {
+function getErrorCallback(res, status) {
     return err => {
-        err.status = httpStatus || err.status;
-        if (err.name && err.name === 'ValidationError') {
-            err.status = HttpStatus.BAD_REQUEST;
-        }
-        res
-            .status(err.status)
-            .send({error: HttpStatus.getStatusText(err.status)});
+        status = err.name === 'ValidationError' ? HttpStatus.BAD_REQUEST : status;
+        sendResponse(res, status, {error: HttpStatus.getStatusText(status), message: err.message});
     };
-}
-
-function throwErrorOnFalseValue(objectToCheck, httpStatus) {
-    if (!objectToCheck) {
-        throw createError(httpStatus);
-    }
 }
 
 function resolveRequestPromise(promise, res, statusCodes = {}) {
-    const successStatus = statusCodes.successCode || HttpStatus.OK;
-    const failureStatus = statusCodes.failureCode || HttpStatus.NOT_FOUND;
+    statusCodes.successCode = statusCodes.successCode || HttpStatus.OK;
+    statusCodes.failureCode = statusCodes.failureCode || HttpStatus.NOT_FOUND;
 
-    return promise
-        .then(getSuccessCallback(res, successStatus))
-        .catch(getErrorCallback(res, failureStatus));
-}
-
-function createError(status, message) {
-    const err = new Error(message);
-    err.status = status;
-
-    return err;
+    return Promise
+        .resolve()
+        .then(promise)
+        .then(getSuccessCallback(res, statusCodes.successCode))
+        .catch(getErrorCallback(res, statusCodes.failureCode));
 }
 
 module.exports = {
     getSuccessCallback,
     getErrorCallback,
-    throwErrorOnFalseValue,
     resolveRequestPromise
 };
