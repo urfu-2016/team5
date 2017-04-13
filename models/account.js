097b9dd5cc37210954b9ad4cdc757dfa59192b30
Alex Solovyev <alexsprint27@mail.ru>
2017-04-13 21:35:30 +0500

    Перенес методы модели из объекта на статические у схемы

--- models/account.js
+++ models/account.js
@@ -26,76 +26,79 @@ const accountSchema = new mongoose.Schema({
     }
 });
 
-const AccountModel = mongoose.model('Account', accountSchema);
 const saltRounds = 10;
 
-module.exports = {
-    create(accountData) {
-        if (!accountData.password) {
-            return Promise.reject(new Error(constants.models.Account.passwordRequiredMessage));
-        }
+function setPasswordAndUser(user, accountData) {
+    return bcrypt
+        .hash(accountData.password, saltRounds)
+        .then(hash => new this({
+            username: accountData.username,
+            password: hash,
+            user: user._id
+        }));
+}
 
-        return User
-            .create({username: accountData.username})
-            .catch(err => {
-                if (err.code === constants.mongoose.mongoDuplicateErrorCode) {
-                    err.message = constants.models.Account.alreadyExistsPattern(accountData.username);
-                }
-
-                throw err;
-            })
-            .then(user => {
-                return bcrypt
-                    .hash(accountData.password, saltRounds)
-                    .then(hash => new AccountModel({
-                        username: accountData.username,
-                        password: hash,
-                        user: user._id
-                    }));
-            })
-            .then(account => {
-                return account
-                    .save()
-                    .catch(err => {
-                        User.removeByUsername(accountData.username);
-
-                        throw err;
-                    });
-            });
-    },
-
-    verifyPassword(account) {
-        return AccountModel
-            .findOne({username: account.username})
-            .exec()
-            .then(acc => acc ? bcrypt.compare(account.password, acc.password) : false);
-    },
-
-    changePassword(account, newPassword) {
-        return this
-            .getAccountOnCorrectPassword(account)
-            .then(acc => {
-                return bcrypt
-                    .hash(newPassword, saltRounds)
-                    .then(hash => {
-                        acc.password = hash;
-
-                        return acc.save();
-                    });
-            });
-    },
-
-    getAccountOnCorrectPassword(account) {
-        return this
-            .verifyPassword(account)
-            .then(verificationResult => {
-                if (!verificationResult) {
-                    throw new Error(constants.models.Account.wrongPasswordOrNameMessage);
-                }
-
-                return AccountModel
-                    .findOne({username: account.username})
-                    .exec();
-            });
+accountSchema.statics.create = function (accountData) {
+    if (!accountData.password) {
+        return Promise.reject(new Error(constants.models.Account.passwordRequiredMessage));
     }
+
+    return User
+        .create({username: accountData.username})
+        .catch(err => {
+            if (err.code === constants.mongoose.mongoDuplicateErrorCode) {
+                err.message = constants.models.Account.alreadyExistsPattern(accountData.username);
+            }
+
+            throw err;
+        })
+        .then(user => setPasswordAndUser.call(this, user, accountData))
+        .then(account => {
+            return account
+                .save()
+                .catch(err => {
+                    User.removeByUsername(accountData.username);
+
+                    throw err;
+                });
+        });
 };
+
+accountSchema.statics.verifyPassword = function (account) {
+    return this
+        .findOne({username: account.username})
+        .exec()
+        .then(acc => acc ? bcrypt.compare(account.password, acc.password) : false);
+};
+
+accountSchema.statics.changePassword = function (account, newPassword) {
+    return this
+        .getAccountOnCorrectPassword(account)
+        .then(acc => {
+            return bcrypt
+                .hash(newPassword, saltRounds)
+                .then(hash => {
+                    acc.password = hash;
+
+                    return acc.save();
+                });
+        });
+};
+
+accountSchema.statics.getAccountOnCorrectPassword = function (account) {
+    return this
+        .verifyPassword(account)
+        .then(verificationResult => {
+            if (!verificationResult) {
+                throw new Error(constants.models.Account.wrongPasswordOrNameMessage);
+            }
+
+            return this
+                .findOne({username: account.username})
+                .exec();
+        });
+};
+
+const AccountModel = mongoose.model('Account', accountSchema);
+
+module.exports = AccountModel;
