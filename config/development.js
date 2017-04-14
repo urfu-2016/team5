d25983c12752f89cfa8c9e5beaef456768eb8f8d
Дмитрий Старков <dstarkdev@gmail.com>
2017-04-14 22:54:00 +0500

    Автодеплой ПР (#94)

--- config/development.js
+++ config/development.js
@@ -1,10 +1,5 @@
 module.exports = {
     port: 8080,
     mode: 'development',
-    mongoUri: 'mongodb://localhost/quests-team5',
-    passportSession: {
-        secret: 'Секретный секрет',
-        resave: false,
-        saveUninitialized: false
-    }
+    mongoUri: 'mongodb://localhost/quests-team5'
 };
