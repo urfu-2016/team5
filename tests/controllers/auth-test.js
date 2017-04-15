41ce23464f42336a8e9a0fe28c81196279b23f15
Мозалов Павел <pp-1995@mail.ru>
2017-04-15 12:48:21 +0600

    Вынес однотипную логику chai-запросов в модуль. (#108)

--- tests/controllers/auth-test.js
+++ tests/controllers/auth-test.js
@@ -1,30 +1,12 @@
 /* eslint-env mocha */
 
-const chai = require('chai');
-const chaiHttp = require('chai-http');
 const server = require('../../app');
 const httpStatus = require('http-status-codes');
 const dbClearer = require('../../scripts/clear-db');
-
-chai.should();
-chai.use(chaiHttp);
+const chaiRequest = require('../commonTestLogic/chaiRequest')(server);
 
 const constants = require('../../constants/constants');
 const mocks = require('../mocks/account');
-
-function signUpAccount(account) {
-    return chai
-        .request(server)
-        .post('/signup')
-        .send(account);
-}
-
-function signInAccount(account) {
-    return chai
-        .request(server)
-        .post('/signin')
-        .send(account);
-}
 
 describe('controller:auth', () => {
     beforeEach(() => dbClearer.removeAll());
@@ -33,13 +15,13 @@ describe('controller:auth', () => {
 
     describe('signup', () => {
         it('should sign up new user', () => {
-            return signUpAccount(mocks.accWithCorrectPassword)
+            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
                 .then(res => res.status.should.be.equal(httpStatus.CREATED));
         });
 
         it('should fail sign up for already used username', () => {
-            return signUpAccount(mocks.accWithCorrectPassword)
-                .then(() => signUpAccount(mocks.accWithCorrectPassword))
+            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
+                .then(() => chaiRequest.post('/signup', mocks.accWithCorrectPassword))
                 .catch(err => {
                     const username = mocks.accWithCorrectPassword.username;
                     const message = constants.models.Account.alreadyExistsPattern(username);
@@ -52,8 +34,8 @@ describe('controller:auth', () => {
 
     describe('signin', () => {
         it('should sign in with correct password', () => {
-            return signUpAccount(mocks.accWithCorrectPassword)
-                .then(() => signInAccount(mocks.accWithCorrectPassword))
+            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
+                .then(() => chaiRequest.post('/signin', mocks.accWithCorrectPassword))
                 .then(res => {
                     const username = mocks.accWithCorrectPassword.username;
                     res.body.data.message.should.equal(constants.controllers.auth.signedInPattern(username));
@@ -61,8 +43,8 @@ describe('controller:auth', () => {
         });
 
         it('should fails sign in with wrong password', () => {
-            return signUpAccount(mocks.accWithCorrectPassword)
-                .then(() => signInAccount(mocks.accWithIncorrectPassword))
+            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
+                .then(() => chaiRequest.post('/signin', mocks.accWithIncorrectPassword))
                 .catch(err => {
                     err.response.body.message.should.equal(constants.models.Account.wrongPasswordOrNameMessage);
                     err.status.should.equal(httpStatus.BAD_REQUEST);
@@ -70,7 +52,7 @@ describe('controller:auth', () => {
         });
 
         it('should fails sign in to non-existent account', () => {
-            return signInAccount(mocks.accWithCorrectPassword)
+            return chaiRequest.post('/signin', mocks.accWithCorrectPassword)
                 .catch(err => {
                     err.status.should.equal(httpStatus.BAD_REQUEST);
                     err.response.body.message.should.equal(constants.models.Account.wrongPasswordOrNameMessage);
