93d755a6e5e61bace345a206ad1ceee4230887d0
FairTeX <makarov906@gmail.com>
2017-04-16 16:38:33 +0500

    починил шапку

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,16 @@ const questsSearch = require('../controllers/questsSearch');
 const Quest = require('../models/quest');
 
 router.get('/', function (req, res) {
+    /*
+    Const renderData = {
+        title: constants.title,
+        isUserAutorized: true,
+        activePage: '/'
+    };
+
+    res.render('mainPage/mainPage', renderData);
+    */
+
     Quest.getAll().then(data => {
         const renderData = {
             title: constants.title,
