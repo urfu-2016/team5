8395fbd1ba84dcf9eb0d953d64cbd918adc15fa8
Antonov Alexey <aleksei193@mail.ru>
2017-04-16 16:45:40 +0500

    Правки

--- views/blocks/SearchBar/params.js
+++ views/blocks/SearchBar/params.js
@@ -49,8 +49,7 @@ module.exports = {
             {
                 from: 'imagesCountFrom',
                 to: 'imagesCountTo'
-            },
-            {}
+            }
         )
     }
 };
