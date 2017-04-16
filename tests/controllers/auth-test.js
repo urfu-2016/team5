7f60a49f916c00d7e1a5f0c605e5a22e3b9a0170
dimastark <dstarkdev@gmail.com>
2017-04-16 18:12:13 +0500

    async

--- tests/controllers/auth-test.js
+++ tests/controllers/auth-test.js
@@ -14,49 +14,52 @@ describe('controller:auth', () => {
     after(() => dbClearer.removeAll());
 
     describe('signup', () => {
-        it('should sign up new user', () => {
-            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
-                .then(res => res.status.should.be.equal(httpStatus.CREATED));
+        it('should sign up new user', async () => {
+            const res = await chaiRequest.post('/signup', mocks.accWithCorrectPassword);
+
+            res.status.should.be.equal(httpStatus.CREATED);
         });
 
-        it('should fail sign up for already used username', () => {
-            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
-                .then(() => chaiRequest.post('/signup', mocks.accWithCorrectPassword))
-                .catch(err => {
-                    const username = mocks.accWithCorrectPassword.username;
-                    const message = constants.models.Account.alreadyExistsPattern(username);
+        it('should fail sign up for already used username', async () => {
+            await chaiRequest.post('/signup', mocks.accWithCorrectPassword);
+            try {
+                await chaiRequest.post('/signup', mocks.accWithCorrectPassword);
+            } catch (err) {
+                const username = mocks.accWithCorrectPassword.username;
+                const message = constants.models.Account.alreadyExistsPattern(username);
 
-                    err.status.should.be.equal(httpStatus.BAD_REQUEST);
-                    err.response.body.message.should.be.equal(message);
-                });
+                err.status.should.be.equal(httpStatus.BAD_REQUEST);
+                err.response.body.message.should.be.equal(message);
+            }
         });
     });
 
     describe('signin', () => {
-        it('should sign in with correct password', () => {
-            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
-                .then(() => chaiRequest.post('/signin', mocks.accWithCorrectPassword))
-                .then(res => {
-                    const username = mocks.accWithCorrectPassword.username;
-                    res.body.data.message.should.equal(constants.controllers.auth.signedInPattern(username));
-                });
+        it('should sign in with correct password', async () => {
+            await chaiRequest.post('/signup', mocks.accWithCorrectPassword);
+            const res = await chaiRequest.post('/signin', mocks.accWithCorrectPassword);
+
+            const username = mocks.accWithCorrectPassword.username;
+            res.body.data.message.should.equal(constants.controllers.auth.signedInPattern(username));
         });
 
-        it('should fails sign in with wrong password', () => {
-            return chaiRequest.post('/signup', mocks.accWithCorrectPassword)
-                .then(() => chaiRequest.post('/signin', mocks.accWithIncorrectPassword))
-                .catch(err => {
-                    err.response.body.message.should.equal(constants.models.Account.wrongPasswordOrNameMessage);
-                    err.status.should.equal(httpStatus.BAD_REQUEST);
-                });
+        it('should fails sign in with wrong password', async () => {
+            await chaiRequest.post('/signup', mocks.accWithCorrectPassword);
+            try {
+                await chaiRequest.post('/signin', mocks.accWithIncorrectPassword);
+            } catch (err) {
+                err.response.body.message.should.equal(constants.models.Account.wrongPasswordOrNameMessage);
+                err.status.should.equal(httpStatus.BAD_REQUEST);
+            }
         });
 
-        it('should fails sign in to non-existent account', () => {
-            return chaiRequest.post('/signin', mocks.accWithCorrectPassword)
-                .catch(err => {
-                    err.status.should.equal(httpStatus.BAD_REQUEST);
-                    err.response.body.message.should.equal(constants.models.Account.wrongPasswordOrNameMessage);
-                });
+        it('should fails sign in to non-existent account', async () => {
+            try {
+                await chaiRequest.post('/signin', mocks.accWithCorrectPassword);
+            } catch (err) {
+                err.status.should.equal(httpStatus.BAD_REQUEST);
+                err.response.body.message.should.equal(constants.models.Account.wrongPasswordOrNameMessage);
+            }
         });
     });
 });
