46575bdbe6ec99e4a1b2d7ace470b8815982f7bd
FairTeX <makarov906@gmail.com>
2017-04-16 15:46:26 +0500

    починил dropdown-menu

--- views/styles/dropdown-menu/dropdown-menu.js
+++ views/styles/dropdown-menu/dropdown-menu.js
@@ -3,7 +3,9 @@
 require('./dropdown-menu.css');
 require('./__item/dropdown-menu__item.css');
 
+console.log('before');
 $('.dropdown-toggle').on('click', function (e) {
+    console.log('in');
     e.preventDefault();
     e.stopPropagation();
 
