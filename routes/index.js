4b7b63f57684c260309165e7f3cb2c9e3e5d2fce
Pavel <pp-1995@mail.ru>
2017-04-23 02:52:58 +0500

    Убрал лишнее в контоллеры

--- routes/index.js
+++ routes/index.js
@@ -3,46 +3,10 @@
 
 const express = require('express');
 const router = express.Router();
-const constants = require('../constants/controllers');
-const questsSearch = require('../controllers/questsSearch');
-const Quest = require('../models/quest');
+const questsController = require('../controllers/quests');
 
-const authController = require('../controllers/api/auth');
-router.get('/', function (req, res) {
-    /*
-    Const renderData = {
-        title: constants.title,
-        activePage: '/',
-        user: {
-            avatar: 'http://www.lovemarks.com/wp-content/uploads/profile-avatars/default-avatar-knives-ninja.png'
-        },
-    };
+router.get('/', questsController.renderAllQuests);
 
-    res.render('mainPage/mainPage', renderData);
-    */
-
-    Quest.getAll().then(data => {
-        console.log(req.session);
-        console.log(req.user);
-        // console.log(req);
-        const renderData = {
-            title: constants.title,
-            user: {
-                avatar: 'http://www.lovemarks.com/wp-content/uploads/profile-avatars/default-avatar-knives-ninja.png'
-            },
-            quests: data.map(q => {
-                q.dateOfCreation = q.dateOfCreation.getDate();
-                return q;
-            }),
-            activePage: '/'
-        };
-
-        res.render('mainPage/mainPage', renderData);
-    });
-});
-
-// authController.authorizedOnly
-
-router.post('/search', authController.authorizedOnly, questsSearch.findQuests);
+router.post('/search', questsController.findQuests);
 
 module.exports = router;
