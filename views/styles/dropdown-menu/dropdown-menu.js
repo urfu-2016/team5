49c3c944686932b30fdfd38f315c4b902dfce4eb
FairTeX <makarov906@gmail.com>
2017-04-29 09:46:12 +0500

    Скрытие dropdown menu при потере фокуса

--- views/styles/dropdown-menu/dropdown-menu.js
+++ views/styles/dropdown-menu/dropdown-menu.js
@@ -6,3 +6,16 @@ require('./__item/dropdown-menu__item.css');
 $('.dropdown-toggle').on('click', function () {
     $(this).parent().toggleClass('show-dropdown-menu');
 });
+
+window.onclick = function(event) {
+    if (!event.target.matches('.avatar__image')) {
+        var dropdowns = document.getElementsByClassName("dropdown-toggle");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.parentNode.classList.contains('show-dropdown-menu')) {
+                openDropdown.parentNode.classList.remove('show-dropdown-menu');
+            }
+        }
+    }
+    event.stopPropagation();
+};
