adfc633d0de05309053f1d76887c93d37db452ed
Dima Starkov <dstarkdev@gmail.com>
2017-05-04 23:50:45 +0500

    Правки стилей

--- views/styles/navbar/navbar.js
+++ views/styles/navbar/navbar.js
@@ -4,6 +4,7 @@ require('./__categories/navbar__categories.css');
 require('./__input/navbar__input.css');
 require('./__item/navbar__item.css');
 require('./__link/navbar__link.css');
+require('./__button/navbar__button.css');
 require('./__navigation-list/navbar__navigation-list.css');
 require('./__toggler/navbar__toggler.css');
 require('./__toggler/navbar__toggler');
