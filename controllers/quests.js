94e188640d5f1bad439243fd14b4c976e1ddb33d
FairTeX <makarov906@gmail.com>
2017-05-07 17:52:31 +0500

    Страница создания квеста

--- controllers/quests.js
+++ controllers/quests.js
@@ -124,7 +124,7 @@ module.exports = {
     },
 
     async renderAllQuests(req, res) {
-        // Const quests = await Quest.getAll();
+        const quests = await Quest.getAll();
         const renderData = {
             title: constants.quest.title,
             isAuth: req.user ? 1 : 0,
