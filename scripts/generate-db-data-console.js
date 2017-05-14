78f79928ef5e8e8d1b0d25a1e8ddd8dcc8984943
Дмитрий Старков <dstarkdev@gmail.com>
2017-05-14 21:18:23 +0500

    Генерация (#208)

--- scripts/generate-db-data-console.js
+++ scripts/generate-db-data-console.js
@@ -7,29 +7,15 @@ const generateDb = require('./generate-db-data');
 const mongoose = require('../libs/mongoose-connection');
 
 const consoleArgs = require('command-line-args')([
-    {name: 'users-count', alias: 'u', type: Number, defaultValue: 1},
-    {name: 'quests-count', alias: 'q', type: Number, defaultValue: 10},
-    {name: 'need-remove', alias: 'r', type: Boolean, defaultValue: true},
-    {name: 'users-file', type: String, defaultValue: path.join(__dirname, '../mocks/users.json')},
-    {name: 'quests-file', type: String, defaultValue: path.join(__dirname, '../mocks/quests.json')}
+    {name: 'quests-file', type: String, defaultValue: path.join(__dirname, 'quests.json')}
 ]);
 
 const questsJson = fs.readFileSync(consoleArgs['quests-file']);
-const usersJson = fs.readFileSync(consoleArgs['users-file']);
-const questsCount = consoleArgs['quests-count'];
-const usersCount = consoleArgs['users-count'];
-const needRemove = consoleArgs['need-remove'];
 
 async function main() {
-    if (needRemove) {
-        await clearDb.dropAll();
-    }
+    await clearDb.dropAll();
 
     await generateDb.createQuestsFromJson(questsJson);
-    await generateDb.createUsersFromJson(usersJson);
-    await generateDb.generateQuests(questsCount);
-    await generateDb.generateUsers(usersCount);
-
     await mongoose.connection.close();
 }
 
--- scripts/generate-db-data.js
+++ scripts/generate-db-data.js
@@ -6,65 +6,26 @@ const Stage = require('../models/stage');
 const constants = require('../constants/generation');
 const shortid = require('shortid');
 
+const createdUsers = [];
+
 function createAll(model, allData) {
     return Promise.all(
         allData.map(data => model.create(data))
     );
 }
 
-async function generateStages({questId, imagesCount = 10}) {
-    const stages = [];
-    const {dummyUrl, imageSize, backgroundColor, foregroundColor} = constants.image;
-    const location = 'default';
-
-    for (let i = 0; i < imagesCount; i++) {
-        let query = `${imageSize}/${backgroundColor}/${foregroundColor}`;
-        let stageData = {
-            title: `title ${i}`,
-            src: `${dummyUrl}/${query}&text=quest+${questId}+${i}`,
-            location
-        };
-        let stage = await Stage.create(stageData);
-
-        stages.push(stage._id);
+function generateUsers(usersCount) {
+    if (usersCount < createdUsers.length) {
+        return createdUsers.slice(0, usersCount);
     }
-
-    return stages;
-}
-
-module.exports.generateQuests = async ({questsCount = 10}) => {
-    const quests = [];
-    const user = await User.create({
-        username: 'User' + Date.now(),
-        email: shortid.generate() + constants.user.email,
-        password: constants.user.password
-    });
-
-    for (let i = 0; i < questsCount; i++) {
-        let questData = {
-            title: `${constants.quest.titlePrefix} ${i}`,
-            description: `${constants.quest.descriptionPrefix} ${i}`,
-            authorId: user._id,
-            slug: `${i}`,
-            city: constants.quest.city,
-            stages: await generateStages({questId: i})
-        };
-
-        quests.push(questData);
-    }
-
-    return createAll(Quest, quests);
-};
-
-module.exports.generateUsers = ({usersCount = 1}) => {
     const users = [];
 
     for (let i = 0; i < usersCount; i++) {
         let userData = {
-            firstname: `${constants.user.firstnamePrefix} ${i} ${shortid.generate()}`,
-            surname: `${constants.user.surnamePrefix} ${i}`,
-            username: `${constants.user.usernamePrefix}${i}`,
-            email: `${i}${constants.user.email}`,
+            firstname: `${constants.user.firstnamePrefix}`,
+            surname: `${constants.user.surnamePrefix}`,
+            username: 'User_' + shortid.generate(),
+            email: shortid.generate() + constants.user.email,
             password: constants.user.password
         };
 
@@ -72,13 +33,7 @@ module.exports.generateUsers = ({usersCount = 1}) => {
     }
 
     return createAll(User, users);
-};
-
-module.exports.createUsersFromJson = json => {
-    const data = JSON.parse(json).data;
-
-    return createAll(User, data);
-};
+}
 
 module.exports.createQuestsFromJson = json => {
     const data = JSON.parse(json).data;
@@ -108,10 +63,21 @@ module.exports.createQuestWithAuthor = async (data, user) => {
         password: constants.user.password
     });
     const stagesIds = [];
+    const likes = [];
 
-    if (data.stageData) {
-        const stage = await Stage.create(data.stageData);
-        stagesIds.push(stage._id);
+    if (data.stages) {
+        for (let stageData of data.stages) {
+            const stage = await Stage.create(stageData);
+            stagesIds.push(stage._id);
+        }
+    }
+
+    if (data.likesCount) {
+        const users = await generateUsers(data.likesCount);
+        createdUsers.concat(users);
+        for (let user of users) {
+            likes.push(user._id);
+        }
     }
 
     return await Quest.create({
@@ -120,6 +86,7 @@ module.exports.createQuestWithAuthor = async (data, user) => {
         description: data.description || '',
         city: data.city || '',
         tags: data.tags || '',
-        stages: stagesIds || []
+        stages: stagesIds || [],
+        likes: likes || []
     });
 };
